c9ad37cddc5d5faf1f8007c1b780ff85d3da06f2
pvb10kir <sehr73@chmail.ir>
2017-07-28 04:50:47 +0430

    Update bot.lua

--- cli/bot/bot.lua
+++ cli/bot/bot.lua
@@ -8,10 +8,10 @@ redis = require('redis')
 database = redis.connect('127.0.0.1', 6379)
 BASE = '/home/cliusp/cliusp/cli/'
 day = 86400
-SUDO = 123456789 --sudo id
-sudo_users = {123456789,userid}
-BOTS = 123456789 --bot id
-bot_id = 123456789
+SUDO = 255317894 --sudo id
+sudo_users = {255317894,userid}
+BOTS = 404973473 --bot id
+bot_id = 404973473
 function vardump(value)
   print(serpent.block(value, {comment=false}))
 end
